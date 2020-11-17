db07fd90711807347aeaf80a04f04acc665fb845
Nick <owennicholasbrown@gmail.com>
2020-11-16 23:42:00 -0500

    Project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,64 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(BEGIN)
+
+   // Probe the keyboard
+   @24576
+   D=M
+   @WHITE
+   D;JEQ
+
+   (BLACK)
+      @val
+      M=-1  // Fills pixels with black
+      @FILL
+      0;JMP
+
+   (WHITE)
+      @val
+      M=0  // Fills pixels with white
+
+   (FILL)
+
+      @SCREEN
+      D=A
+      @idx
+      M=D
+      @8192 // # registers in the screen : 265 bits * 512 bits / 16 bits per register
+      D=D+A
+      @end
+      M=D
+
+      (LOOPFILL)
+
+         // First, check if we've reached the end of the screen
+         // Jump to the top of the program if so.
+         @end
+         D=M
+         @idx
+         D=D-M
+         @BEGIN
+         D;JLE
+
+         // Fill this register with the value set by the keyboard press
+         @val
+         D=M
+         @idx
+         A=M
+         M=D
+
+         // Move on to the next register
+         @idx
+         M=M+1
+
+         // Jump back to the beginning of the program if so
+         @LOOPFILL
+         0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,37 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@R0
+D=M
+@iter
+M=D
+
+@R2
+M=0
+
+(LOOP)
+  // Check if we've finished the loop; jump if so
+  @iter
+  D=M
+  @END
+  D;JEQ
+
+  // Add R1 to the value in R2
+  @R2
+  D=M
+  @R1
+  D=D+M
+  @R2
+  M=D
+
+  // Decrement iter
+  @iter
+  M=M-1
+
+  @LOOP
+  0;JMP
+
+(END)
+   @END
+   0;JMP
